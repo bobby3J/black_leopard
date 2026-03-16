ac76a0041ed67b36d1447acdec26ab66d10e8214
Robert Ackon <jaybobby320@gmail.com>
2026-03-16 14:58:29 -0700

    Update footer cookie section with list and asterisk bullets, and remove Cart.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -10,30 +10,39 @@ const Footer = () => {
 <div class="box-container">
 
    <div class="box">
-        <h3>our locations</h3>
+        <h3>Our Locations</h3>
     <a href="#"> <i class="fas fa-map-marker-alt"></i> Eastern Region - Koforidua</a>
     <a href="#"> <i class="fas fa-map-marker-alt"></i> Greater Accra - Accra</a>
    </div>
 
 <div class="box">
-        <h3>quick links</h3>
+        <h3>Quick Links</h3>
     <a href="#"> <i class="fas fa-arrow-right"></i> home </a>
     <a href="#about"> <i class="fas fa-arrow-right"></i> about </a>
-    <a href="#services"> <i class="fas fa-arrow-right"></i> services </a>
-    <a href="#projects"> <i class="fas fa-arrow-right"></i> projects </a>
+    <a href="#services"> <i class="fas fa-arrow-right"></i> services </a>  
+</div>
 
-  
-   </div>
-
-   <div class="box">
-    <h3>extra links</h3>
+   {/* <div class="box">
+    <h3>Extra Links</h3>
 <a href="#"> <i class="fas fa-arrow-right"></i> account info </a>
 <a href="#"> <i class="fas fa-arrow-right"></i> privacy policy </a>
 <a href="#"> <i class="fas fa-arrow-right"></i> payment method </a>
+</div> */}
+
+
+   <div class="box">
+    <h3>Cookies</h3>
+    <ul>
+        <li>We may use cookies to improve your experience</li>
+        <li>Cookies help us remember your preferences</li>
+        <li>You can disable cookies in your browser settings</li>
+        <li>Some features may not work properly without cookies</li>
+    </ul>
+
 </div>
 
    <div class="box">
-    <h3>contact</h3>
+    <h3>Contact</h3>
 <a href="#"> <i class="fas fa-phone"></i> +233 509 957 073 </a>
 <a href="#"> <i class="fas fa-envelope"></i> support@blackleopardtech.com 
  </a>
@@ -43,11 +52,11 @@ const Footer = () => {
 </div>
 
 <div class="share">
-<a href="#" class="fab fa-facebook"></a>
-<a href="#" class="fab fa-twitter"></a>
-<a href="#" class="fab fa-instagram"></a>
-<a href="#" class="fab fa-linkedin"></a>
-<a href="#" class="fab fa-pinterest"></a>
+{/* <a href="#" class="fab fa-facebook"></a> */}
+{/* <a href="#" class="fab fa-twitter"></a> */}
+<a href="https://www.instagram.com/blackleopardtech" class="fab fa-instagram"></a>
+<a href="https://www.linkedin.com/company/blackleopard-technologies-limited" class="fab fa-linkedin"></a>
+<a href="https://www.tiktok.com/@blackleopardtech" class="fab fa-tiktok"></a>
 
 </div>
 
